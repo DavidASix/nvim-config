9f7f8c157079565882bdbe5fa73b4a9068b77668
davidasix <david@davidasix.com>
2026-05-23 12:22:10 -0400

    Install bufferline and remap some keys

--- lua/davidasix/remapper.lua
+++ lua/davidasix/remapper.lua
@@ -6,3 +6,10 @@ map("n", "<leader>o", "<cmd>Telescope find_files<cr>")
 -- grep through the whole folder tree, returning the files where the search term appears
 map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
 
+-- Navigate buffers
+map("n", "<leader><Left>", "<cmd>BufferLineCyclePrev<cr>")
+map("n", "<leader><Right>", "<cmd>BufferLineCycleNext<cr>")
+
+-- Close current buffer
+map("n", "<leader>w", "<cmd>bd<cr>")
+
