0bdb29f039718dc12dc49153d735870a6ce0f209
davidasix <david@davidasix.com>
2026-05-23 12:21:45 -0400

    Move relative line numbers and add comments

--- lua/davidasix/init.lua
+++ lua/davidasix/init.lua
@@ -2,4 +2,7 @@ vim.g.mapleader = " " -- Set leader key before Lazy
 require("davidasix.init_lazy")
 require("davidasix.remapper")
 
+-- Add relative line numbers
+vim.wo.relativenumber = true
+-- Apply the color scheme
 vim.cmd.colorscheme("tokyonight")
