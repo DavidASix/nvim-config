7c8279eaf5768df27cfdfd55ff265a367f6345a1
davidasix <david@davidasix.com>
2026-05-23 10:29:27 -0400

    Initial Commit

---
+++ lua/davidasix/init.lua
@@ -0,0 +1,5 @@
+vim.g.mapleader = " " -- Set leader key before Lazy
+require("davidasix.lazy_init")
+require("davidasix.remapper")
+
+vim.cmd.colorscheme("tokyonight")
---
+++ lua/davidasix/remapper.lua
@@ -0,0 +1,6 @@
+
+local map = vim.keymap.set
+
+map("n", "<leader>o", "<cmd>Telescope find_files<cr>")
+map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
+
