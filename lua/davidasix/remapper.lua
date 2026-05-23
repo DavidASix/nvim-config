073a12e3c7ade1a8e3c242596e67c63a86678970
davidasix <david@davidasix.com>
2026-05-23 11:56:39 -0400

    Add comments

--- lua/davidasix/remapper.lua
+++ lua/davidasix/remapper.lua
@@ -1,6 +1,8 @@
 
 local map = vim.keymap.set
 
+-- Search for files by path / file name and open them
 map("n", "<leader>o", "<cmd>Telescope find_files<cr>")
+-- grep through the whole folder tree, returning the files where the search term appears
 map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
 
