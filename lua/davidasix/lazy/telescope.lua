da4ce58baeebfc5b7fbbb8018956587e7e10b2a8
davidasix <david@davidasix.com>
2026-05-23 15:16:48 -0400

    Move key commands into plugin

--- lua/davidasix/lazy/telescope.lua
+++ lua/davidasix/lazy/telescope.lua
@@ -1,5 +1,10 @@
 return {
     'nvim-telescope/telescope.nvim',
     tag = 'v0.2.0',
-    dependencies = { 'nvim-lua/plenary.nvim' }
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    keys = {
+        { "<leader>o",  "<cmd>Telescope find_files<cr>",  desc = "Find files" },
+        { "<leader>fg", "<cmd>Telescope live_grep<cr>",   desc = "Live grep" },
+        { "<leader>gs", "<cmd>Telescope git_status<cr>",  desc = "Git status" },
+    },
 }
--- lua/davidasix/remapper.lua
+++ lua/davidasix/remapper.lua
@@ -1,15 +1,11 @@
 
 local map = vim.keymap.set
 
--- Search for files by path / file name and open them
-map("n", "<leader>o", "<cmd>Telescope find_files<cr>")
--- grep through the whole folder tree, returning the files where the search term appears
-map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
-
 -- Navigate buffers
 map("n", "<leader><Left>", "<cmd>BufferLineCyclePrev<cr>")
 map("n", "<leader><Right>", "<cmd>BufferLineCycleNext<cr>")
 
 -- Close current buffer
 map("n", "<leader>w", "<cmd>bd<cr>")
+
 
